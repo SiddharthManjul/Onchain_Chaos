bcd8bda83b0ee8a8594185729789a807de2b0f5f
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-06-17 23:35:23 +0530

    Smooth Cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../custom/hero";
 import DefinitionBox from "../custom/definitionBox";
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
 
 export default function Home() {
   return (
     <div>
+      <SmoothCursor />
       <Hero />
       <DefinitionBox />
     </div>
