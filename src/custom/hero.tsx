8ddf11d9fcd5a44c040e050e891dab4be866ac0a
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-06-30 23:59:36 +0530

    Hero Section -> BG Change

--- src/custom/hero.tsx
+++ src/custom/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div
       className="h-[120vh] w-screen bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: "url('/hero-bg-2.jpg')" }}
+      style={{ backgroundImage: "url('/hero-bg.jpg')" }}
     >
       {/* Centered content over background */}
       <div className="h-[80vh] w-[80vw] flex items-center justify-center rounded-bl-xl rounded-tr-xl border-4 backdrop-blur-xs">
